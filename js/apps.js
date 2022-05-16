e5e860448dcbac3f1712631814d7a1905562f25a
joseolinda <joseolindad@yahoo.com.br>
2022-05-16 20:45:06 -0300

    HTML e CSS base

---
+++ js/apps.js
@@ -0,0 +1,30 @@
+const btnAddTarefa = document.querySelector("#add_tarefa")
+const lstAFazer = document.querySelector("#a_fazer")
+const lstFeito = document.querySelector("#feito")
+
+const adicionarTarefa = function() {
+    const tarefa = document.querySelector("#tarefa").value
+
+    if (tarefa.length < 1) {
+        return;
+    }
+
+    const novaTarefa = document.createElement("li")
+    novaTarefa.innerText = tarefa
+
+    lstAFazer.appendChild(novaTarefa)
+}
+
+const moverParaFeito = function(ev) {
+    const tarefa = ev.target.innerText
+
+    const novaTarefa = document.createElement("li")
+    novaTarefa.innerText = tarefa
+
+    lstFeito.appendChild(novaTarefa)
+    lstAFazer.removeChild(ev.target)
+}
+
+btnAddTarefa.addEventListener("click", adicionarTarefa)
+
+lstAFazer.addEventListener("click", moverParaFeito)
