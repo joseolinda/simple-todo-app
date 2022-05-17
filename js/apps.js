7b586c867edc904b331faf2f99e19673891fa24c
joseolinda <joseolindad@yahoo.com.br>
2022-05-17 14:45:40 -0300

    Melhora na responsividade

--- js/apps.js
+++ js/apps.js
@@ -1,8 +1,10 @@
 const btnAddTarefa = document.querySelector("#add_tarefa")
-const lstAFazer = document.querySelector("#a_fazer")
+const lstAFazer = document.querySelector("#a-fazer")
 const lstFeito = document.querySelector("#feito")
 
-const adicionarTarefa = function() {
+const adicionarTarefa = function(ev) {
+    ev.preventDefault()
+    
     const tarefa = document.querySelector("#tarefa").value
 
     if (tarefa.length < 1) {
