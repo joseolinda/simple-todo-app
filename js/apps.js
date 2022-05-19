df4e7ba3b049d2a54aca6e3f07e80a4e261b67da
Jose Olinda <joseolindad@yahoo.com.br>
2022-05-19 19:13:25 -0300

    Dark mode v0.1

--- js/apps.js
+++ js/apps.js
@@ -221,3 +221,26 @@ if (localStorage.getItem('lista-feito')) {
 btnAddTarefa.addEventListener("click", adicionarTarefa)
 
 lstAFazer.addEventListener("click", moverParaFeito)
+
+
+// Dark mode
+const toggle = document.getElementById("dark-mode");
+
+const storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme) {
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    document.querySelector(".container-switch span").value = storedTheme
+}
+
+
+toggle.onchange = function() {
+    let currentTheme = document.documentElement.getAttribute("data-theme");
+    let targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
